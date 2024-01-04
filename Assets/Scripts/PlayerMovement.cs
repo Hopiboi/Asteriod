242c4eba0fc7d3239be3e431d0be5ea135dcb933
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-01-04 18:34:33 +0800

    Asteriod

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rigidbody2d;
+
+    [Header ("PlayerMovement")]
+    [SerializeField] private float thrustSpeed = 2f;
+    [SerializeField] private float rotationSpeed = 2f;
+    [SerializeField] private float turnDirection;
+    [SerializeField] private bool _thrustMovement;
+
+    void Awake()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _thrustMovement = Input.GetKey(KeyCode.W);
+
+        //left
+        if (Input.GetKey(KeyCode.A))
+        {
+            turnDirection = 1.0f;
+        }
+        //right
+        else if (Input.GetKey(KeyCode.D))
+        {
+            turnDirection = -1.0f;
+        }
+        //neutral
+        else
+        {
+            turnDirection = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_thrustMovement)
+        {
+            rigidbody2d.AddForce(this.transform.up * thrustSpeed);
+        }
+
+        if (turnDirection != 0f)
+        {
+            rigidbody2d.AddTorque(turnDirection * rotationSpeed);
+        }
     }
 }
